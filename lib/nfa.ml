ef316ae9d4b59254cfabc8d132fdd25901baa099
Ernest Ng <eyn5@cornell.edu>
2025-05-19 11:53:12 -0400

    Format code

--- lib/nfa.ml
+++ lib/nfa.ml
@@ -1,25 +1,24 @@
 type state = int32
-module StateSet = Set.Make(Int32)
-module CharMap = Map.Make(Char)
+
+module StateSet = Set.Make (Int32)
+module CharMap = Map.Make (Char)
+
 type transitions = StateSet.t CharMap.t
 
 type nfa = {
-  start : StateSet.t;
-  (** the start states *)
-
-  finals: StateSet.t;
-  (** the final (or "accept") states *)
-
-  next: state -> transitions;
-  (** the transition function, that maps a state and a character to a
+  start : StateSet.t;  (** the start states *)
+  finals : StateSet.t;  (** the final (or "accept") states *)
+  next : state -> transitions;
+    (** the transition function, that maps a state and a character to a
       set of states *)
 }
 
 let find_states sym nfa m =
-  try CharMap.find sym (nfa.next m)
-  with Not_found -> StateSet.empty
+  try CharMap.find sym (nfa.next m) with Not_found -> StateSet.empty
 
-let flat_map f ss = StateSet.fold (fun s -> StateSet.union (f s)) ss StateSet.empty
+let flat_map f ss =
+  StateSet.fold (fun s -> StateSet.union (f s)) ss StateSet.empty
+
 let nextss curs sym nfa = flat_map (find_states sym nfa) curs
 
 (** A simple NFA interpreter. *)
@@ -31,5 +30,5 @@ let accept nfa inp =
       states *)
   let rec step cur = function
     | [] -> StateSet.(not (is_empty (inter cur nfa.finals)))
-    | c :: cs -> step (nextss cur c nfa) cs
-  in step nfa.start inp
+    | c :: cs -> step (nextss cur c nfa) cs in
+  step nfa.start inp
--- lib/regex.mli
+++ lib/regex.mli
@@ -1,48 +1,47 @@
 (** Regular expression combinators *)
 
-type t
 (** The type of regular expressions *)
+type t
 
-type charset = Set.Make(Char).t
 (** Sets of characters *)
+type charset = Set.Make(Char).t
 
-val empty : t
 (** [empty] rejects every string *)
+val empty : t
 
-val eps : t
 (** [eps] accepts only the empty string *)
+val eps : t
 
-val any : t
 (** [any] accepts any single character *)
+val any : t
 
-val oneof : charset -> t
 (** [oneof cs] accepts any character in cs *)
+val oneof : charset -> t
 
-val range : char -> char -> t
 (** [range l h] accepts any character in the range [l]..[h] *)
+val range : char -> char -> t
 
-val chr : char -> t
 (** [chr c] accepts exactly the character [c] *)
+val chr : char -> t
 
-val seq : t -> t -> t
 (** [seq x y] accepts strings [rs] where [x] accepts [r] and [y]
     accepts [s] *)
+val seq : t -> t -> t
 
-val alt : t -> t -> t
 (** [alt x y] accepts any string accepted by either [x] or [y] *)
+val alt : t -> t -> t
 
-val opt : t -> t
 (** [opt r] accepts any string accepted by [r], and the empty string *)
+val opt : t -> t
 
-val star : t -> t
 (** [star r] accepts any string consisting of zero or more copies of
     a string accepted by [r] *)
+val star : t -> t
 
-val plus : t -> t
 (** [star r] accepts any string consisting of one or more copies of
     a string accepted by [r] *)
+val plus : t -> t
 
-val parse : string -> t
 (** Parse a regular expression using the following grammar:
 
       r ::= (r)          (parenthesized regex)
@@ -57,14 +56,15 @@ val parse : string -> t
 
    Raises [Parse_error] on parse error
 *)
+val parse : string -> t
 
-val unparse_charset : charset -> string
 (** [unparse_charset cs] is a string denoting a regular expression
     that accepts any character in [cs], and nothing else *)
+val unparse_charset : charset -> string
 
-val compile : t -> Nfa.nfa
 (** [compile r] translates [r] to an NFA that succeeds on exactly
     those strings matched by [r] *)
+val compile : t -> Nfa.nfa
 
+(** Raised when [parse] is given an invalid regex *)
 exception Parse_error of string
-(** Raised when [parse] is given an invalid regex *) 
